97be7bfffc0bbd14234d1d4bdb45000a65f3d7bc
Iavorskyi Oleksandr <a7038329@gmail.com>
2022-10-17 15:20:47 +0300

    test

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -5,7 +5,7 @@ export default {
     actions: {
         login({commit, state}){
             const body = {email: state.email, password: state.password}
-            axios.post('http://s3gallery-svc.dev.svc.cluster.local/sign-in', body)
+            axios.post('http://s3gallery-svc.dev/sign-in', body)
                 .then((res)=> {
                     commit('updateToken', res.data.token);
                     commit('updateUser', res.data.user)})
