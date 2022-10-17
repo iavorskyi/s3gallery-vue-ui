0a08c7e611b2a5aa45e7d0d60f3d13088f0645da
Iavorskyi Oleksandr <a7038329@gmail.com>
2022-10-17 18:07:50 +0300

    test

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  devServer: {
-    proxy: {
-      '': {
-        target: 'http://s3gallery:8000',
-        changeOrigin: true
-      },
-    }
-  }
 })
